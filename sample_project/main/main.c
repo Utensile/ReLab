da53c1f7ab89a260f082748a91b98c87b2e562a2
Utensile <132955875+Utensile@users.noreply.github.com>
2023-06-13 09:55:49 +0200

    Sample project update

--- sample_project/main/main.c
+++ sample_project/main/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
 
 void app_main(void)
 {
+    int i=0;
     while(1){
-        printf("ciao monte");
+        printf("%d\n", i);
+        vTaskDelay(40 / portTICK_PERIOD_MS);
+        i+=40;
     }
 }
