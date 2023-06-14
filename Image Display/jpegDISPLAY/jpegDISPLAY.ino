b2a6e9ef3062a8bd43eed8b44dbdd2662bb8acce
aleancio <136358649+aleancio@users.noreply.github.com>
2023-06-14 16:20:36 +0200

    upload image

--- Image Display/jpegDISPLAY/jpegDISPLAY.ino
+++ Image Display/jpegDISPLAY/jpegDISPLAY.ino
@@ -4,12 +4,12 @@
 #include "images.h"
 
 #define IMG_NUM 8
-int pos=0;
+int pos = 0;
 WROVER_KIT_LCD tft;
-const char* ssid     = "RE:Lab";
+const char* ssid = "RE:Lab";
 const char* password = "Interact2019!";
 
-const int len[IMG_NUM]      = {zanas_len, mario_len, beacon_len, black_hole_len, earth_len, orbit_len, rocket_len, rover_len};
+const int len[IMG_NUM] = {zanas_len, mario_len, beacon_len, black_hole_len, earth_len, orbit_len, rocket_len, rover_len};
 const uint8_t* img[IMG_NUM] = {zanas, mario, beacon, black_hole, earth, orbit, rocket, rover};
 
 WiFiServer server(80);
@@ -31,8 +31,8 @@ void setup() {
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
+    delay(500);
+    Serial.print(".");
   }
 
   Serial.println("");
@@ -40,18 +40,18 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   server.begin();
-
 }
 
 void loop() {
   WiFiClient client = server.available();   // listen for incoming clients
 
-  if (client) {                             // if you get a client,          // print a message out the serial port
+  if (client) {                             // if you get a client
     String currentLine = "";                // make a String to hold incoming data from the client
     while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then                  // print it out the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character a response:
+      if (client.available()) {             // if there's bytes to read from the client
+        char c = client.read();             // read a byte
+
+        if (c == '\n') {                    // if the byte is a newline character
           if (currentLine.length() == 0) {
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
@@ -60,34 +60,52 @@ void loop() {
             client.print("a{-webkit-appearance: button; -moz-appearance: button; appearance: button; text-decoration: none; color: initial; color: #fff; background-color: #4CAF50; border: none; padding: 15px 75px; font-size: 30px; border-radius: 5px; cursor: pointer; transition: background-color 0.3s ease;}.button-container{display: flex; justify-content: center; gap: 10px; margin-top: 10vh; margin-bottom: 40px;}a:hover{background-color: #45a049;}</style>");
             client.print("<body><div class=\"button-container\"><a href=\"/L\" type=\"button\">Previous Image</a><a href=\"/H\" type=\"button\">Next Image</a></div>");
 
-            // The HTTP response ends with another blank line:
             client.println();
-            // break out of the while loop:
             break;
-          } else {    // if you got a newline, then clear currentLine:
+          } else {
             currentLine = "";
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+        } else if (c != '\r') {
+          currentLine += c;
         }
 
         if (currentLine.endsWith("GET /H")) {
           pos++;
-          pos=pos%IMG_NUM;
+          pos = pos % IMG_NUM;
+          extToInt();
+          delay(5000);
           tft.setRotation(1);
+          tft.cl
           tft.drawJpg(img[pos], len[pos]);
         }
         if (currentLine.endsWith("GET /L")) {
-          pos--; 
-          if(pos<0)
-            pos=IMG_NUM-1;
+          extToInt();
+          delay(5000);
+          pos--;
+          if (pos < 0)
+            pos = IMG_NUM - 1;
           tft.setRotation(1);
           tft.drawJpg(img[pos], len[pos]);
         }
-        
+
       }
     }
-    // close the connection:
     client.stop();
+  }
+}
+
+unsigned long extToInt() {
+  unsigned long start;
+  int w, i, i2,
+      cx = tft.width() / 2 - 1,
+      cy = tft.height() / 2 - 1;
+
+  tft.fillScreen(WROVER_BLACK);
+  w = min(tft.width(), tft.height());
+  start = micros();
+  for (i = 0; i < w; i += 6) {
+    i2 = i / 2;
+    tft.drawRoundRect(cx - i2, cy - i2, i, i, i / 8, tft.color565(i, 0, 0));
+    delay(50);
   }
 }
