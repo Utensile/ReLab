07fa9df2b59865c781bdc53816eaa76aaf2a1aa8
aleancio <136358649+aleancio@users.noreply.github.com>
2023-06-15 16:46:12 +0200

    Uplad Immagini completo

---
+++ Image Display/Esp_Prova_upload/Esp_Prova_upload.ino
@@ -0,0 +1,112 @@
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
+#include "SPIFFS.h"
+#include "FS.h"
+#include "Adafruit_GFX.h"
+#include "WROVER_KIT_LCD.h"
+
+
+const char* ssid = "RE:Lab";
+const char* password = "Interact2019!";
+
+
+#define IMG_NUM 8
+int pos = 0;
+WROVER_KIT_LCD tft;
+
+AsyncWebServer server(80);
+
+
+
+
+void setup() {
+  Serial.begin(115200);
+  
+
+  // Montaggio della memoria SPIFFS
+  if (!SPIFFS.begin()) {
+    Serial.println("Errore nel montaggio di SPIFFS");
+    return;
+  }
+  //inizializzazione canvas
+  tft.begin();
+  tft.setRotation(1);
+  // Connessione alla rete WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+   // Serial.println("Connessione in corso alla rete WiFi...");
+  }
+
+  Serial.println("Connessione WiFi stabilita");
+  Serial.print("Indirizzo IP: ");
+  Serial.println(WiFi.localIP());
+  File root = SPIFFS.open("/");
+  File file = root.openNextFile();
+  while (file) {
+    // Stampa il nome di ogni file nella directory radice
+    if(file.name() != ""){
+      Serial.println(file.name());
+      file = root.openNextFile();
+    }
+    else{
+      Serial.println("Probleam");
+    }
+  }
+  root.close();
+  
+  // Configurazione del server web
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
+
+  
+
+  // Gestione della richiesta di caricamento immagine
+  server.on(
+    "/upload",
+    HTTP_POST,
+    [](AsyncWebServerRequest *request){
+      AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", "Caricamento immagine completato");
+      response->addHeader("Access-Control-Allow-Origin", "*");
+      request->send(response);
+      Serial.println("BreakPoint_1");
+    },
+    [](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
+
+      if (!index) {
+        // Apertura del file per il caricamento iniziale
+        File file = SPIFFS.open("/immagine.jpg", FILE_WRITE);
+        if (!file) {
+          Serial.println("Errore nell'apertura del file");
+          return;
+        }
+        file.write(data, len);
+        file.close();
+       } else {
+        // Caricamento successivo del file
+        File file = SPIFFS.open("/immagine.jpg", FILE_APPEND);
+        if (!file) {
+          Serial.println("Errore nell'apertura del file");
+          return;
+        }
+        file.write(data, len);
+        file.close();
+      }
+      if (final) {
+        Serial.println("Caricamento immagine completato");
+        tft.drawJpgFile(SPIFFS, "/immagine.jpg", 0, 0);
+      }
+    }
+  );
+
+  // Inizializzazione del server
+  server.begin();
+}
+
+void loop() {
+  // Codice di gestione delle altre operazioni, se necessario
+
+}
